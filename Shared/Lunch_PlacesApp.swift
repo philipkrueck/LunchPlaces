f068d297cfae4765dc2fe7ce52299a922fc8a2ff
Philip Krück <philip.krueck@apploft.de>
2020-06-24 15:03:31 +0200

    Initial Commit

---
+++ Shared/Lunch_PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lunch_PlacesApp.swift
+//  Shared
+//
+//  Created by Philip Krück on 24.06.20.
+//
+
+import SwiftUI
+
+@main
+struct Lunch_PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
