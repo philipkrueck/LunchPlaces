8235ceab974047390ef4ffc17036688b935c3e65
Philip Krück <philip.krueck@apploft.de>
2020-06-25 21:59:07 +0200

    added assets

---
+++ Shared/OpenMapButton.swift
@@ -0,0 +1,20 @@
+//
+//  OpenMapButton.swift
+//  Lunch Places
+//
+//  Created by Philip Krück on 25.06.20.
+//
+
+import SwiftUI
+
+struct OpenMapButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OpenMapButton_Previews: PreviewProvider {
+    static var previews: some View {
+        OpenMapButton()
+    }
+}
